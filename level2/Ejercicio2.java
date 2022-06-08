0e1b8f260f416e3c929f5ca313087ba10faf65d6
Ezequiel-Manfredi <ezemanfre@gmail.com>
2022-06-08 16:55:34 -0300

    refactor and add list length

--- level2/Ejercicio2.java
+++ level2/Ejercicio2.java
@@ -3,15 +3,23 @@ package level2;
 import java.util.ArrayList;
 
 public class Ejercicio2 {
+    public static void print(ArrayList<Integer> numbers) {
+        for (Integer integer : numbers) {
+            System.out.print(integer + " ");
+        }
+        System.out.println("\nlength: " + numbers.size());
+    }
     public static void main(String[] args) {
         ArrayList<Integer> numbers = new ArrayList<>();
         for (int i = 2; i <= 6; i++) {
             numbers.add(i);
         }
     
+        print(numbers);
+    
         numbers.add(0,1);
         numbers.add(numbers.size(),7);
 
-        System.out.println(numbers);
+        print(numbers);
     }
 }
